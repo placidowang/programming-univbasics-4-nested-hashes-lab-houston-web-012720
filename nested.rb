8482bf1dd894c242f19db351586737250c6b4b3b
Placido Wang <placidow@gmail.com>
2020-01-20 22:18:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -96,8 +96,6 @@ def adding_matz
   programmer_hash
 end
 
-adding_matz
-
 def changing_alan
 	programmer_hash = 
  		{
@@ -116,6 +114,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
      
 end
