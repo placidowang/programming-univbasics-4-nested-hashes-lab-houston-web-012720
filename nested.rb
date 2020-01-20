b83473d4d05c4fa2b938b765cd597305194ed9f9
Placido Wang <placidow@gmail.com>
2020-01-20 22:11:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,10 +88,11 @@ def adding_matz
      }
   
   programmer_hash[:yukihiro_matsumoto] =
-    :yukihiro_matsumoto => {
-      :known_for => "Ruby",
-      :languages => ["LISP", "C"]
-    }
+    :key => "value"
+    # :yukihiro_matsumoto => {
+    #   :known_for => "Ruby",
+    #   :languages => ["LISP", "C"]
+    # }
     
   programmer_hash
 end
